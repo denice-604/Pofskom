7846d24229792a040067248f12a7c13d0a0189e7
Denho <Denho@DESKTOP-CEVI7C0>
2023-03-08 21:40:32 +0300

    подключил entity, добавил комменты свойств членов

--- Kursach.BL/Model/Member.cs
+++ Kursach.BL/Model/Member.cs
@@ -1,12 +1,144 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Kursach.BL.Model
 {
-    internal class member
+    internal class Member
     {
+        #region свойства
+
+        /// <summary>
+        /// Member Id.
+        /// </summary>
+        public int Id { get; }
+
+        #region стабильное
+        /// <summary>
+        /// Member name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Member surename.
+        /// </summary>
+        public string Surename { get; set; }
+
+        /// <summary>
+        /// Member patronymic.
+        /// </summary>
+        public string Patronymic { get; set; }
+
+        /// <summary>
+        /// Member gender.
+        /// </summary>
+        public bool Gender { get; set; }
+        
+        /// <summary>
+        /// Member phine numper.
+        /// </summary>
+        public string PhoneNumber { get; set; }
+
+        /// <summary>
+        /// Member marital status.
+        /// </summary>
+        public bool MaritalStatus { get; set; }
+
+        /// <summary>
+        /// Member's date of birth.
+        /// </summary>
+        public DateTime BirthDate { get; set; }
+        #endregion 
+
+        #region прописка по документу
+        /// <summary>
+        /// Member house number. (documents) 
+        /// </summary>
+        public string NumberHouseD { get; set; }
+
+        /// <summary>
+        /// Member's residence street. (documents)
+        /// </summary>
+        public string AddressD { get; set; }
+
+        /// <summary>
+        /// Member's city of residence. (documents)
+        /// </summary>
+        public string CityD { get; set; }
+
+        /// <summary>
+        /// Member's area of residence. (documents)
+        /// </summary>
+        public string RegionD { get; set; }
+
+        /// <summary>
+        /// Member country of residence. (documents)
+        /// </summary>
+        public string CountryD { get; set; }
+        #endregion
+
+        #region прописка по факту
+        /// <summary>
+        /// Member house number.
+        /// </summary>
+        public string NumberHouse { get; set; }
+
+        /// <summary>
+        /// Member's residence street. 
+        /// </summary>
+        public string Address { get; set; }
+
+        /// <summary>
+        /// Member's city of residence.
+        /// </summary>
+        public string City { get; set; }
+
+        /// <summary>
+        /// Member's area of residence.
+        /// </summary>
+        public string Region { get; set; }
+
+        /// <summary>
+        /// Member country of residence. 
+        /// </summary>
+        public string Country { get; set; }
+        #endregion
+
+        #region рабочее
+
+        /// <summary>
+        /// Member post.
+        /// </summary>
+        public string Post { get; set; }
+
+        /// <summary>
+        /// Member photo title.
+        /// </summary>
+        public string Photo { get; set; }
+
+        /// <summary>
+        /// Member education.
+        /// </summary>
+        public string Education { get; set; }
+
+        /// <summary>
+        /// Member email address.
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Member's file number.
+        /// </summary>
+        public string PersonalFileNumber { get; set; }
+
+        /// <summary>
+        /// Member citizenship.
+        /// </summary>
+        public string Citizenship { get; set; }
+
+        #endregion
+
+        #endregion
+
+        public Member() { }
+
     }
 }
