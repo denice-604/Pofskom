1fca6dd790f2f128858ddba8b26cf74c621f4803
Denho <Denho@DESKTOP-CEVI7C0>
2023-03-09 20:43:21 +0300

    добавил класс контекста для entity и пара свойств для членства

---
+++ Kursach.BL/MemberContext.cs
@@ -0,0 +1,9 @@
+﻿using System.Data.Entity;
+
+namespace Kursach.BL
+{
+    internal class DbConnect : DbContext
+    {
+        protected DbConnect() : base("DbConnect") { }
+    }
+}
--- Kursach.BL/Model/Membership.cs
+++ Kursach.BL/Model/Membership.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace Kursach.BL.Model
 {
     internal class Membership
     {
+        public int Id { get; set; }
+        public DateTime DateJoin { get; set; }
+        public string Name { get; set; }
     }
 }
